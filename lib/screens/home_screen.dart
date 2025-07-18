b1e6520f60e217ef42c18f700fefe19fee1229f6
Doniyorgit config --global user.name <doniyorumurzakov2009@gmail.com>
2025-07-18 10:45:21 +0300

    .

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -57,6 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
    backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
